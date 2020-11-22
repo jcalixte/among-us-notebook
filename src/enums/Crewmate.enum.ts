443fbdfb506b13b162b1f0e8f39c3dc29200ce39
Julien Calixte <juliencalixte@gmail.com>
2020-11-22 12:08:20 +0100

    Initial commit

---
+++ src/enums/Crewmate.enum.ts
@@ -0,0 +1,12 @@
+export enum Crewmate {
+  Red = '#c23616',
+  Blue = '#0097e6',
+  Green = '#44bd32',
+  Yellow = '#fbc531',
+  Orange = '#fa8231',
+  Black = '#2d3436',
+  White = '#ffffff',
+  Purple = '#8854d0',
+  Cyan = '#34e7e4',
+  Lime = '#b8e994'
+}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import '@/registerServiceWorker'
+
+import App from './App.vue'
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+import { messages } from '@/locales/message'
+import { router } from '@/router/router'
+
+const i18n = createI18n({
+  locale: 'fr',
+  messages
+})
+
+createApp(App)
+  .use(router)
+  .use(i18n)
+  .mount('#app')

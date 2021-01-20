0da60933f030d2266a6a8c94447af2fb7dd171ff
Julien Calixte <julienc@bam.tech>
2021-01-20 12:29:46 +0100

    :sparkles: (app)

---
+++ src/components/Notebook.vue
@@ -0,0 +1,62 @@
+<template>
+  <button v-if="hasGameStarted" class="button" @click="clear">
+    Nouvelle partie
+  </button>
+  <div class="notebook">
+    <div v-for="crewmate in crewmates" :key="crewmate" class="crewmate">
+      <Crewmate
+        :color="crewmate"
+        :is-suspected="suspects[crewmate] > 0"
+        @suspect="suspect"
+      />
+      <div class="gommettes">
+        <div v-for="suspected in suspects[crewmate]" :key="suspected">
+          🔴
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import { useNotebook } from './useNotebook.hook'
+import Crewmate from '@/components/Crewmate.vue'
+
+export default defineComponent({
+  name: 'Notebook',
+  components: {
+    Crewmate
+  },
+  setup() {
+    const { crewmates, suspects, suspect, clear } = useNotebook()
+    const hasGameStarted = computed(() =>
+      Object.values(suspects).some((s) => s > 0)
+    )
+
+    return {
+      crewmates,
+      suspects,
+      suspect,
+      clear,
+      hasGameStarted
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.notebook {
+  .crewmate {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .gommettes {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+    }
+  }
+}
+</style>
---
+++ src/components/useNotebook.hook.ts
@@ -0,0 +1,44 @@
+import { Crewmate } from '@/enums/Crewmate.enum'
+import { computed, reactive } from 'vue'
+
+const initialCrewmates = [
+  Crewmate.Red,
+  Crewmate.Blue,
+  Crewmate.Green,
+  Crewmate.Yellow,
+  Crewmate.Orange,
+  Crewmate.Black,
+  Crewmate.White,
+  Crewmate.Purple,
+  Crewmate.Cyan,
+  Crewmate.Lime
+]
+
+type Suspects = Record<Crewmate, number>
+
+const initialSuspects = initialCrewmates.reduce((acc, crewmate) => {
+  acc[crewmate] = 0
+  return acc
+}, {} as Suspects)
+
+export const useNotebook = () => {
+  const suspects = reactive({ ...initialSuspects })
+  const crewmates = computed(() =>
+    initialCrewmates.filter((crewmate) => suspects[crewmate] < 3)
+  )
+  const suspect = (crewmate: Crewmate) => {
+    suspects[crewmate]++
+  }
+  const clear = () => {
+    for (const crewmate of initialCrewmates) {
+      suspects[crewmate] = 0
+    }
+  }
+
+  return {
+    crewmates,
+    suspects,
+    suspect,
+    clear
+  }
+}
